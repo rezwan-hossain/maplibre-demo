11c5a752e5831df68d9e15510f796fc3e1af1696
Rezwan Hossain <rezwanvoot@gmail.com>
2026-01-09 21:58:58 +0600

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Route, Switch } from "wouter";
+import { Route, Router, Switch } from "wouter";
 import MapBox from "./components/MapBox";
 import CsvFile from "./components/CsvFile";
+import { useHashLocation } from "wouter/use-hash-location";
 
 function App() {
   return (
     <>
-      <Switch>
-        <Route path="/" component={MapBox} />
-        <Route path="/csv" component={CsvFile} />
-      </Switch>
+      <Router hook={useHashLocation}>
+        <Switch>
+          <Route path="/" component={MapBox} />
+          <Route path="/csv" component={CsvFile} />
+        </Switch>
+      </Router>
     </>
   );
 }
